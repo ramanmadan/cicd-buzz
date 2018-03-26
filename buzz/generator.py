db13d40b1fa27e2bbbcb3f6fe24a3dc89ec2d88c
ramanmadan <Raman.Madan@gmail.com>
2018-03-26 17:12:31 -0400

    initial load

---
+++ buzz/generator.py
@@ -0,0 +1,27 @@
+import random
+
+buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
+
+adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
+
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+
+def sample(l, n=1):
+    result = random.sample(l, n)
+    if n ==l:
+        return result[0]
+    return result[0]
+
+def generate_buzz():
+    buzz_terms = sample(buzz, 2)
+##    phrase = ' '.join([sample(adjectives,1)])
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), sample(verbs), buzz_terms[1]])
+    return phrase.title()
+
+if __name__ == "__main__":
+    print (generate_buzz())
+else:
+    print ('testing')
+
